454c9ae91c3c12918284cfa4faee821072a9b63b
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2024-09-14 22:00:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/lk3140318/MdiskSearchBotV2/blob/main/app.json /Auto_Filter_A_2_Z_Bot
-cd /Auto_Filter_A_2_Z_Bot
+cd /@Auto_Filter_A_2_Z_Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
